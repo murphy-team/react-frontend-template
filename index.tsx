42e0a7b9b76c4cbe981c7a4736bfd463ff4b5a08
Álvaro Gómez <alvaro.gomez@grupoal.eu>
2018-01-11 16:16:44 +0100

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,53 @@
+// import * as React from "react";
+// import * as ReactDOM from "react-dom";
+// import {IndexRoute, Route, Router} from "react-router";
+// import {replace, routeActions, routerActions} from "react-router-redux";
+// import {AppPipeline} from "./src/components/AppPipeline";
+// import * as injectTapEventPlugin from "react-tap-event-plugin";
+// import {SubSequenceSearchPageContainer} from "./src/pages/SubSequenceSearch/SubSequenceSearchPageContainer";
+// import {LanguageBS} from "./src/access-data/bs/LanguageBS";
+// import {RoutesConstants} from "./src/common/RoutesConstants";
+// import {UploadFileToProcessingPageContainer} from "./src/pages/UploadFilesToProcessing/UploadFileToProcessingPageContainer";
+// import {DownloadPageContainer} from "./src/pages/Download/DownloadPageContainer";
+// import {SettingsContainer} from "./src/pages/Settings/SettingsContainer";
+// import {DatabaseSubSeqSearchPageContainer} from "./src/pages/DatabaseSubSeqSearch/DatabaseSubSeqSearchPageContainer";
+// import {LoginRegisterPageContainer} from "./src/pages/Login/LoginRegisterPageContainer";
+// import {history} from "./src/components/AppPipeline";
+// import {userIsAuthenticated, userIsNotAuthenticated} from "./src/access-data/bs/AuthBS";
+//
+// require('./assets/images/favicon.ico');
+//
+// class Index {
+//     public constructor() {
+//
+//         injectTapEventPlugin();
+//         const Start = () => (
+//             <Router history={history}>
+//                 <Route path="/" component={AppPipeline}>
+//                     <IndexRoute component={userIsAuthenticated(SubSequenceSearchPageContainer)}/>
+//                     <Route path={RoutesConstants.DATABASE_SEARCH_ROUTE_PATH}
+//                            component={userIsAuthenticated(DatabaseSubSeqSearchPageContainer)}/>
+//                     <Route path={RoutesConstants.UPLOAD_FILES_ROUTE_PATH}
+//                            component={userIsAuthenticated(UploadFileToProcessingPageContainer)}/>
+//                     <Route path={RoutesConstants.DOWNLOAD_FILES_ROUTE_PATH}
+//                            component={userIsAuthenticated(DownloadPageContainer)}/>
+//                     <Route path={RoutesConstants.SETTINGS_ROUTE_PATH}
+//                            component={userIsAuthenticated(SettingsContainer)}/>
+//                     <Route path={RoutesConstants.LOGIN_ROUTE_PATH}
+//                            component={userIsNotAuthenticated(LoginRegisterPageContainer)}/>
+//                 </Route>
+//             </Router>
+//         );
+//
+//         ReactDOM.render(
+//             <Start/>
+//             ,
+//             document.getElementById('root')
+//         );
+//
+//     }
+// }
+//
+//
+// new Index();
+//
